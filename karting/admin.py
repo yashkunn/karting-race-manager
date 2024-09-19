1d52fba2b6ba7e7f2072f59f4480d80186ec6a6f
Yakiv Lobach <lobachyakiv@gmail.com>
2024-09-19 18:57:42 +0300

    admin panel settings

--- karting/admin.py
+++ karting/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import (
+    Race, 
+    RaceCategory, 
+    Kart, 
+    CustomUser, 
+    RaceParticipation
+)
+
+
+@admin.register(Race)
+class RaceAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "date")
+    list_filter = ("category",)
+    search_fields = ("category__name", "name")
+
+
+@admin.register(RaceCategory)
+class RaceCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "min_age", "max_age")
+    search_fields = ("name",)
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("age",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("age",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "age",
+                        "email",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(RaceParticipation)
+class RaceParticipationAdmin(admin.ModelAdmin):
+    list_display = ("user", "race", "kart", "date_registered")
+    list_filter = ("race", "kart", "date_registered")
+    search_fields = ("user__username", "race__name", "kart__name")
+    date_hierarchy = "date_registered"
+
+
+@admin.register(Kart)
+class KartAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "speed")
+    list_filter = ("category", "speed")
+    search_fields = ("name", "category__name")
+    
