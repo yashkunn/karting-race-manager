613df694e183bbd415e07c2ec4c0362580de73f8
Yakiv Lobach <lobachyakiv@gmail.com>
2024-09-23 21:22:24 +0300

    update settings, add requirementstxt

--- karting_race_manager/settings.py
+++ karting_race_manager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%sb1))4+#ign5tdt@v*bt%6%w()bd9u9n52wo2ermdkg8$%7s%"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-%sb1))4+#ign5tdt@v*bt%6%w()bd9u9n52wo2ermdkg8$%7s%")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
