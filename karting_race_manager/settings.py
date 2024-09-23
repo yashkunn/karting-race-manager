1335745b3d4cda098943c24d7d6d1442a58b5d4a
Yakiv Lobach <lobachyakiv@gmail.com>
2024-09-24 01:42:43 +0300

    change link in ALLOWED_HOSTS

--- karting_race_manager/settings.py
+++ karting_race_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://karting-race-manager.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "karting-race-manager.onrender.com"]
 
 
 # Application definition
